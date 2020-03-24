416f420982edfd68c50ccf76d159ffc49eb11302
Sean McClannahan <sean1@iastate.edu>
2020-03-24 13:37:36 -0500

    first commit; added a short product list

---
+++ products/products.js
@@ -0,0 +1,25 @@
+var products = [
+	{"name":"Apple",
+	"type":"fruit",
+	"price":2},
+	
+	{"name":"Banana",
+	"type":"fruit"
+	"price":1},
+	
+	{"name":"potato",
+	"type":"vegetable",
+	"price":2},
+	
+	{"name":"celery",
+	"type": "vegetable",
+	"price":2},
+	
+	{"name":"coffee",
+	"type":"drink"
+	"price":3},
+	
+	
+	
+	
+]
