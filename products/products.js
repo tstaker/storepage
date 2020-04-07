e4bdccb2bbff8f5581e70a11557d46ee8c253a49
Sean McClannahan <sean1@iastate.edu>
2020-04-07 12:32:28 -0500

    prices are now ints so they can be worked with easier

--- products/products.js
+++ products/products.js
@@ -2,58 +2,54 @@
 var products = [
 	{"name":"Apple",
 	"type":"fruit",
-	"price":"$2.00"},
+	"price":2},
 	
 	{"name":"Banana",
 	"type":"fruit",
-	"price":"$1.00"},
+	"price":1},
 	
 	{"name":"Beef",
 	"type":"meat",
-	"price":"$5.00"},
+	"price":5},
 	
 	{"name":"Celery",
 	"type": "vegetable",
-	"price":"$2.00"},
+	"price":2},
 	
 	{"name":"Cheese",
 	"type":"dairy",
-	"price":"$3.00"},
+	"price":3},
 	
 	{"name":"Chicken",
 	"type":"meat",
-	"price":"$4.00"},
+	"price":4},
 
 	{"name":"Chips",
 	"type":"other",
-	"price":"$3.00"},
+	"price":3},
 	
 	{"name":"Coffee",
 	"type":"drink",
-	"price":"$3.00"},
+	"price":3},
 	
 	{"name":"Milk",
 	"type":"dairy",
-	"price":"$4.00"},
+	"price":4},
 	
 	{"name":"Pork",
 	"type":"meat",
-	"price":"$4.00"},
+	"price":4},
 		
 	{"name":"Potato",
 	"type":"vegetable",
-	"price":"$2.00"},
+	"price":2},
 		
 	{"name":"Soda",
 	"type":"drink",
-	"price":"$2.00"}
+	"price":2}
 	
 ];
-var cart = [
-	{"name": null,
-	"type":null,
-	"price":null}
-];
+var cart = [];
 function products(){
 	products.forEach(generateNameAndPrice);
 }
@@ -63,17 +59,28 @@ function getType(x){
 }
 
 function addToCart(x){
-	cart.push(products[x-1]);
+	var item = products[x];
+	cart.push({name: item.name, price: item.price});
 }
 function checkOut(){
 	
+}
+function moneyFormat(x){
+	var item = products[x-1];
+	var money = (item.price).toLocaleString('en-US', {style:'currency', currency: 'USD',});
+	return money;
+}
+function noArrayFormat(x){
+	var money = (x).toLocaleString('en-US', {style:'currency', currency: 'USD',});
+	return money;
 }
 
 function generateNameAndPrice(x){
 	var item = products[x-1];
 	document.getElementById("itemName" + x).innerHTML = item.name;
 	document.getElementById("itemName" + x).style.fontSize = "xx-large";
-	document.getElementById("itemPrice" + x).innerHTML = item.price;
+	var price = moneyFormat(x);
+	document.getElementById("itemPrice" + x).innerHTML = price;
 	document.getElementById("itemPrice" + x).style.fontSize = "xx-large";
 }
 
@@ -81,14 +88,20 @@ function checkOut(){
 	window.location.href = "./checkOut.html";
 }
 
+
+
 function getCart(){
-	for(let i = 1;i<cart.length;++i){
+	var itemDisplay = '';
+	var totalDisplay = '';
+	var total = 0;
+	for(var i = 0;i<cart.length;++i){
 		var item = cart[i];
-		var itemDisplay = "<p>";
-		itemDisplay = itemDisplay + item.name + " - " + item.price;
-		itemDisplay = itemDisplay + "</p>";
-		document.getElementById("cart").appendChild(itemDisplay);	
+			itemDisplay += "<p>" + item.name + " - " + item.price + "</p>" + "<br>";
+			total += item.price;
 		}
+	totalDisplay = noArrayFormat(total);
+	itemDisplay += "Total: " + totalDisplay;
+	document.getElementById("cart").innerHTML = itemDisplay;
 	
 }
 
