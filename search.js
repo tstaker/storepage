d06867b8d7a6ddb148e4f2d710b6d1807a64c377
ttstaker <ttstaker@iastate.edu>
2020-04-19 23:01:36 +0000

    Updated search.js for testing purposes

--- search.js
+++ search.js
@@ -2,6 +2,10 @@
 //This function requires that the items in the products
 //list are in exactly the same order as the gui cells
 
+//List of variables for Jest testing
+export var displayed = "block";
+export var item1Shown = true;
+
 var submitThis = function(){
 	var products = [
 	{"name":"apple",
@@ -56,16 +60,21 @@ var submitThis = function(){
 	var item = document.getElementById("searchtext");
 	var filter = item.value.toLowerCase();
 	var count = 0;
+	item1Shown = true;
 	for(var i = 0; i < products.length; i++){
 		//Hides all products that don't have the same type as the one being searched for
 		if(products[i].type != filter && products[i].name != filter && products[i].price != filter){
 			document.getElementById("item" + (i+1).toString()).style.display = "none";
+			if(i === 0){
+				item1Shown = false;
+			}
 		}
 		else{
 			document.getElementById("item" + (i+1).toString()).style.display = "block";
 			count++;
 		}
 	}
+	displayed = document.getElementById("item1").style.display;
 	if(count === 0){
 	    alert("No results found");
 	}
@@ -126,7 +135,9 @@ function home(){
 
 	getChange(3); /*un-does any filters the client may have currently applied*/
 
+    item1Shown = true;
 	for(var i = 0; i < products.length; i++){
 		document.getElementById("item" + (i+1).toString()).style.display = "block";
 	}
+	displayed = document.getElementById("item1").style.display;
 }
