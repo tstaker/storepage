5bcb78b11cc65a43b619b6ce89d8adc0c0782d9e
ttstaker <ttstaker@iastate.edu>
2020-04-28 22:38:22 +0000

    Update searchTest.test.js

--- Testing/searchTest.test.js
+++ Testing/searchTest.test.js
@@ -1,9 +1,11 @@
 //Must have Jest for this to work!
-import {item1Result} from './searchTest';
-import {item1Shown, displayed} from '../search.js';
+import {item1Shown, displayed, submitThis} from './testOne.js';
 
-describe('item1Result', () => {
-	it('correctly adjusted based on the search function', () =>{
-		expect(item1Result(displayed)).toBe(item1Shown);
+//Change this depending on what value you want to test for
+submitThis("2");
+
+describe('SpecifiedItem', () => {
+	it('is shown', () =>{
+		expect(item1Shown).toBe(true);
 	})
 })
