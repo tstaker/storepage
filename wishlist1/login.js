dcc839fc635f1c8bd6696ffb040f5ef3f681c9e1
derrickv <derrickv@iastate.edu>
2020-05-01 15:00:50 +0000

    Removed unnecessary comments

--- wishlist1/login.js
+++ wishlist1/login.js
@@ -41,60 +41,6 @@ function registerCheck() {
 
 }
 
-// var registrationInfo = [
-// 	{
-// 		username: derrick,
-// 		password: vang
-// 	}
-// ]
-
-// function signIn() {
-// 	var modal = document.getElementById('login');
-
-// 	window.onclick = function(event) {
-// 		if (event.target == modal) {
-// 			modal.style.display = "none";
-// 		}
-// 	}
-// }
-
 function signUp() {
-	// var x = document.getElementById("register");
-	// if (x.style.display === "none") {
-	//   x.style.display = "block";
-	// } else {
-	//   x.style.display = "none";
-	// }
 	window.location.href = "../welcomePage/register.html";
 }
-
-// function registerCheck() {
-
-// 	var initialUsername = document.getElementById('username2').value;
-// 	var firstPassword = document.getElementById('password2').value;
-// 	//var secondPassword = document.getElementById('password3').value;
-// 	var newUser = {
-// 		username: initialUsername,
-// 		password: firstPassword
-// 	}
-
-// 	// if(firstPassword == secondPassword) {
-// 	// 	window.alert("Your username is " + initialUsername + " and your password is " + firstPassword);
-// 	// 	registrationInfo.push(newUser);
-// 	// 	return
-// 	// }
-// 	registrationInfo.push(newUser);
-// 	//window.alert("Incorrect password. Try again.");
-// }
-
-// function loginCheck() {
-// 	var usernamen = document.getElementById('username1').value;
-// 	var passwordn = document.getElementById('password1').value;
-// 	for(i = 0; i < registrationInfo.length; ++i){
-// 	if(usernamen == registrationInfo[i].username && passwordn == registrationInfo[i].password){
-// 		window.alert(usernamen + " is logged in!");
-// 		return
-// 		}
-// 	}
-// 	window.alert("Incorrect username or password. Try again.");
-// }
