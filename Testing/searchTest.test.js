6c782341a6f00b2e94f63b8ef12c6e7fae245816
ttstaker <ttstaker@iastate.edu>
2020-05-01 03:39:42 +0000

    Increased comment information

--- Testing/searchTest.test.js
+++ Testing/searchTest.test.js
@@ -2,6 +2,7 @@
 import {item1Shown, displayed, submitThis} from './testOne.js';
 
 //Change this depending on what value you want to test for
+//In this example, testing for all values with a price of 2
 submitThis("2");
 
 describe('SpecifiedItem', () => {
